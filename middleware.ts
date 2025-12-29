5cc10b3f0b85496a90c2c508e812245c11be53e2
Tasbihanoor <noortasbiha5@gmail.com>
2025-12-29 15:49:40 +0500

    error solved

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { verifyToken } from './src/lib/auth';
+import { verifyToken } from '@/lib/auth';
 
 export function middleware(request: NextRequest) {
   // Public routes that don't require authentication
